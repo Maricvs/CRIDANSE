75d7b9b70ce872f3ce5ce5dadcda1757e174fda9
Maricvs <maricvs@gmail.com>
2025-03-31 23:02:27 +0300

    sa

--- backend/main.py
+++ backend/main.py
@@ -12,5 +12,5 @@ app = FastAPI()
 # Затем подключаем остальные роутеры
 app.include_router(gpt_api.router, prefix="/api/gpt")
 app.include_router(auth.router, prefix="/api/auth")
-app.include_router(chat.router, prefix="/api/chat")
+app.include_router(chat.router, prefix="/api/chats")
 
