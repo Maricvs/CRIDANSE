a0eb4d33f65ea962d8c319ff6bebc8cf967d3dd5
Maricvs <maricvs@gmail.com>
2025-03-31 21:04:19 +0300

    check

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@
 
 from fastapi import FastAPI
 from api import gpt_api, auth, chat
-from app.components.mylibrary.documents import router as documents_router
+
 
 
 app = FastAPI()
@@ -12,4 +12,4 @@ app.include_router(documents_router, prefix="/api/documents")
 # Затем подключаем остальные роутеры
 app.include_router(gpt_api.router, prefix="/api/gpt")
 app.include_router(auth.router, prefix="/api/auth")
-app.include_router(chat.router, prefix="/api/chats")
+
