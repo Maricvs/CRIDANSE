65885943eed371821049b552cdd32edb81d28eb2
Maricvs <maricvs@gmail.com>
2025-03-31 03:00:47 +0300

    Удалены тестовые файлы и конфигурации для фронтенда и бэкенда, включая .env.test, init_db.py, requirements.txt, и тесты для компонентов Auth и Chat.

--- backend/init_db.py
+++
@@ -1,9 +0,0 @@
-# backend/init_db.py
-
-from db import engine
-from models.models import Base
-
-if __name__ == "__main__":
-    print("⏳ Создание таблиц...")
-    Base.metadata.create_all(bind=engine)
-    print("✅ Таблицы успешно созданы.")
