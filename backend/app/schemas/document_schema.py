8cc1c6e578e79e79a7dc55fbeedf7ed1a37b2c5f
Maricvs <maricvs@gmail.com>
2025-04-10 01:51:30 +0300

    добавили user_name: Optional[str] в json

--- backend/app/schemas/document_schema.py
+++ backend/app/schemas/document_schema.py
@@ -19,6 +19,7 @@ class DocumentUpdate(DocumentBase):
 class Document(DocumentBase):
     id: int
     user_id: int
+    user_name: Optional[str]
     file_name: str
     file_size: int
     file_path: str
