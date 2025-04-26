011e9e69ef1c61314b8c46408918e692c031a239
Maricvs <maricvs@gmail.com>
2025-04-26 15:35:31 +0300

    Добавлена типизация для глобального использования axiosInstance

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,6 +8,16 @@ import axiosInstance from './services/axiosInstance'
 // Инициализация axiosInstance уже происходит в самом файле
 // Токен будет автоматически добавлен при создании инстанса
 
+// Добавляем типизацию для глобального использования
+declare global {
+  interface Window {
+    axiosInstance: typeof axiosInstance;
+  }
+}
+
+// Делаем axiosInstance доступным глобально
+window.axiosInstance = axiosInstance;
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <GoogleOAuthProvider clientId="541044049283-5dj1189l8nm3mq3o77ij41k79kb69kle.apps.googleusercontent.com">
