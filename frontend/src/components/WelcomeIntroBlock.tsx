19d905d2558c182477bb0bf0baf82624ecbc8da3
Maricvs <maricvs@gmail.com>
2026-03-17 08:19:13 +0200

    Update

--- frontend/src/components/WelcomeIntroBlock.tsx
+++ frontend/src/components/WelcomeIntroBlock.tsx
@@ -6,8 +6,8 @@ const WelcomeIntroBlock: React.FC = () => {
   return (
     <div className="welcome-intro-block">
 
-      <h2 className="welcome-title">Hi, I'm CRIDANSE.</h2>
-      <p className="welcome-subtitle">Your personal learning assistant</p>
+      <h2 className="welcome-title">Hi, I'm CRIDANSE!</h2>
+      <p className="welcome-subtitle">Your personal learning assistant2</p>
     </div>
   );
 };
