cf0c4205b6c2e5c21aba0b647a97ae08ef2e991e
Maricvs <maricvs@gmail.com>
2026-03-17 08:30:40 +0200

    Test preprod full deploy

--- frontend/src/components/WelcomeIntroBlock.tsx
+++ frontend/src/components/WelcomeIntroBlock.tsx
@@ -7,7 +7,7 @@ const WelcomeIntroBlock: React.FC = () => {
     <div className="welcome-intro-block">
 
       <h2 className="welcome-title">Hi, I'm CRIDANSE!</h2>
-      <p className="welcome-subtitle">Your personal learning assistant2</p>
+      <p className="welcome-subtitle">Your personal learning assistant</p>
     </div>
   );
 };
